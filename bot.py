cf5c76f9316a2dac2599534f28fcebf83a12e8be
Abigail <138245554+KitsunAbigail@users.noreply.github.com>
2025-04-01 15:41:51 -0400

    make sure nothing goes awry

--- bot.py
+++ bot.py
@@ -17,11 +17,11 @@ from dotenv import load_dotenv
 intents = discord.Intents.default()
 intents.message_content = True
 load_dotenv()
-TOKEN = os.getenv('hahaha you thought you would get the actual token for the bot')
-APPLICATION_ID = os.getenv('hahaha you thought you would get the actual token for the bot')
+TOKEN = os.getenv('PRETEND_THIS_IS_THE_TOKEN')
+APPLICATION_ID = os.getenv('PRETEND_THIS_IS_THE_ID')
 
 bot = commands.Bot(command_prefix="🍰", intents=intents, application_id=APPLICATION_ID)
-developer_id = pretendthisismyid  # Replace with your own user ID
+developer_id = "my id"  # Replace with your own user ID
 
 # Check if the user is the developer
 async def is_developer(interaction: discord.Interaction):
@@ -45,8 +45,8 @@ async def meow(interaction: discord.Interaction):
 
     response = random.randint(1,4)
     letters = random.randint(2, 7)
-
-    match(response):
+    reply = ""
+    match response:
         case 1:
             reply = "meow" + ("w" * (letters // 2)) + ("!" * letters)
         case 2:
@@ -60,21 +60,28 @@ async def meow(interaction: discord.Interaction):
 @bot.event
 async def on_ready():
 
-    print(f'{bot.user} has connected to Discord!')
+    logging.info(f'{bot.user} has connected to Discord!')
     await bot.tree.sync()
 
     # Only sync commands for the developer
     if bot.user.id == developer_id:
         await bot.tree.sync()
-        print("Commands have been synced.")
+        logging.info("Commands have been synced.")
 
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="VOCALOID music ^w^"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="videos about voice synthesizers .w."))
 
 # Echo command that is "hidden" for other users
 @bot.tree.command(name="console", description="might have some secret commands")
 @app_commands.describe(message="enter smth")
 async def echo(interaction: discord.Interaction, message: str):
-    if await is_developer(interaction) in message:
+    if "beans" in message:
+        await interaction.response.send_message("hehehe ;3", ephemeral=True)
+        await interaction.channel.send("yes beans")
+    elif "do j" in message or "guys help" in message:
+        await interaction.response.send_message("hehehe ;3", ephemeral=True)
+        await interaction.channel.send("guys help")
+        await interaction.channel.send("do j")
+    elif await is_developer(interaction):
         await interaction.response.send_message("hehehe ;3", ephemeral=True)
         await interaction.channel.send(message)
     else:
@@ -87,7 +94,16 @@ async def vocaloid(interaction: discord.Interaction):
              "https://youtu.be/sV2H712ldOI", "https://youtu.be/kbNdx0yqbZE", "https://youtu.be/-eDYT_20YhM", "https://youtu.be/LaEgpNBt-bQ",
              "https://youtu.be/NocXEwsJGOQ", "https://youtu.be/M7VSEZOQIlg", "https://youtu.be/vjBFftpQxxM", "https://youtu.be/HsZQOXZbWkQ"
              ]
+
     response = random.randint(1, len(songs))
     await interaction.response.send_message(songs[response - 1])
 
-bot.run('hahaha you thought you would get the actual token for the bot')
+@bot.tree.command(name="utauloid", description="tetoris")
+async def utauloid(interaction: discord.Interaction):
+    songs = ["https://youtu.be/yZkPe7TEuyk", "https://youtu.be/dbfFDAHmP_c", "https://youtu.be/_AjJZEcMdww", "https://youtu.be/01xG-Ggx6EE",
+             "https://youtu.be/1U6qefKcOrg", "https://youtu.be/_SRp1kc0088", "https://youtu.be/Soy4jGPHr3g"]
+
+    response = random.randint(1, len(songs))
+    await interaction.response.send_message(songs[response - 1])
+
+bot.run(PRETEND_THIS_IS_THE_TOKEN)
